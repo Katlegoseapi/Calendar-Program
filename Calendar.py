7a2fb56b5d7b8d1b7446a347bf03405f725e479e
Katlego Seapi <seapikatlego@gmail.com>
2024-03-18 13:58:38 +0200

    first commit

---
+++ Calendar.py
@@ -0,0 +1,73 @@
+print("calandar 2024")
+import calendar
+yy=2024
+mm=1
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=2
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=3
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=4
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=5
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=6
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=7
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=8
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=9
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=10
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=11
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=12
+
+print(calendar.month(yy,mm))
+import calendar
+yy=2024
+mm=13
+
+
+
+
+
+
+
+
+
